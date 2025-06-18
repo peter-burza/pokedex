429de8a7a370185e83292e16d4aba6132fbde830
Peter Burza <peterburza48@gmail.com>
2025-06-18 07:24:16 +0000

    Added filtering of pokemons after typing its name or number into the search bar.

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,20 +1,35 @@
 import { first151Pokemon, getFullPokedexNumber } from "../utils"
+import { useState } from 'react'
 
 export default function SideNav(props) {
     const { selectedPokemon, setSelectedPokemon } = props
+    const [searchValue, setSearchValue] = useState('')
+    const filteredPokemon = first151Pokemon.filter((ele, eleIndex) => {
+        // If full pokemon name includes the current search value, return true
+        if (getFullPokedexNumber(eleIndex).includes(searchValue)) return true
+
+        // If the pokemon name includes the current search name, return true
+        if(ele.toLowerCase().includes(searchValue.toLowerCase())) return true
+
+        // Otherwise, exclude value from the array
+        return false
+    })
 
     return (
         <nav>
             <div className={"header"}>
                 <h1 className="text-gradient">Pokédex</h1>
             </div>
-            <input type="text" />
-            {first151Pokemon.map((pokemon, pokemonIndex) => {
+            <input placeholder="E.g. 001 or Bulba..." value={searchValue}  onChange={(e) => {
+                setSearchValue(e.target.value)
+            }}/>
+            {filteredPokemon.map((pokemon, pokemonIndex) => {
+                const truePokedexNumber = first151Pokemon.indexOf(pokemon)
                 return (
                     <button onClick={() => {
-                        setSelectedPokemon(pokemonIndex)
-                    }} key={pokemonIndex} className={'nav-card ' + (pokemonIndex === selectedPokemon ? 'nav-card-selected' : '')}> 
-                        <p>{getFullPokedexNumber(pokemonIndex)}</p>
+                        setSelectedPokemon(truePokedexNumber)
+                    }} key={truePokedexNumber} className={'nav-card ' + (truePokedexNumber === selectedPokemon ? 'nav-card-selected' : '')}>
+                        <p>{getFullPokedexNumber(truePokedexNumber)}</p>
                         <p>{pokemon}</p>
                     </button>
                 )
